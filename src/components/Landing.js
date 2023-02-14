96fee3ce088a59e6fc431e3d4e89be6ce6e3cde0
saeed jahedi nasab <saeedjahedinasab@gmail.com>
2023-02-14 13:10:06 +0330

    Mapping on data

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -3,11 +3,15 @@ import React, { useEffect, useState } from 'react';
 import { getCoin } from '../services/api';
 //Components
 import Loader from './Loader';
+import Coin from './Coin';
+
+//Styles
+import styles from "./Landing.module.css";
 
 const Landing = () => {
 
     const [coins, setCoins] = useState([]);
-
+    const[search, setSearch] = useState("")
     useEffect(()=>{
         const fetchAPI = async ()=>{
             const data = await getCoin();
@@ -17,14 +21,28 @@ const Landing = () => {
         fetchAPI();
     }, [])
 
+    const searchHandler = event =>{
+        setSearch(event.target.value)
+    }
+
+    const searchCoins = coins.filter(coin => coin.name.toLowerCase().includes(search.toLowerCase()))
+
     return (
         <>
-            <input type="text" placeholder='Search' />
+            <input className={styles.input} type="text" placeholder='Search' value={search} onChange={searchHandler} />
             {
                 coins.length ?
-                <div>
+                <div className={styles.coinContainer}>
                 {
-                    coins.map(coin =><p key={coin.id}>{coin.name}</p>)
+                   searchCoins.map(coin => <Coin 
+                        key={coin.id}   
+                        name={coin.name}                       
+                        image={coin.image}
+                        symbol={coin.symbol}
+                        price={coin.current_price}
+                        marketCap={coin.market_cap}
+                        priceChange={coin.price_change_percentage_24h}
+                        />)
                 }
                 </div> :
                 <Loader />
